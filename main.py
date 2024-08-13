466b5d28275fe14f5eb3a4974a876f311a8a356a
osvaldo <osvaldobone16@gmail.com>
2024-08-13 07:07:25 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de dados")
+
+#variaveis
+idade = 43
+nome = "Joaquim"
+
+# print
+print(idade)
+
+#soma
+nova_idade = idade + 10
+print(nova_idade)       
+
+# lista
+lista = [1,2,3,4,5,6]
+
+#metodo
+lista_invertida = lista.reverse()
